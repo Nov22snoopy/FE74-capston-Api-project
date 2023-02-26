24fdcc577756f156679f9a87c39cd55e73f0e9c5
HyhTonHappy <tanphuchuynh2547@gmail.com>
2023-02-26 11:53:15 +0700

    commit
    commit

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -18,13 +18,14 @@ getListProduct();
             content += `
             <tr>
              <td>${i+1}</td>
-             <td>${product.tenSP}</td>
-              <td>${product.gia}</td>
+             <td>${product.name}</td>
+              <td>${product.price}</td>
               <td>${product.screen}</td>
-              <td>${product.bCamera}</td>
+              <td>${product.backCamera}</td>
+              <td>${product.frontCamera}</td>
 <td>
-<img scr="./img/${product.hinhAnh} " width = "50""/>
-<td>${product.moTa}</td>
+<img scr="./img/${product.img} " width = "50""/>
+<td>${product.desc}</td>
 <td>${product.type}</td>
 </td>
 <button class="btn btn-info" data-toggle="modal" data-target="#myModal" onclick="handleEdit(${product.id})">Edit</button>
