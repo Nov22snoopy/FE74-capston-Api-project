29a6b25dbf8a13efe279941e82dc178308c33657
HyhTonHappy <tanphuchuynh2547@gmail.com>
2023-02-27 07:00:13 +0700

    sua

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -2,6 +2,7 @@ var callApi = new CallApi();
 function getEle(id){
     return document.getElementById(id);
 } 
+var hinhAnhFromServer = "";
 getListProduct();   
   function getListProduct(){
     callApi.fetchListData()
@@ -36,3 +37,91 @@ getListProduct();
         });
         getEle("tblDanhSachSP").innerHTML = content;
   }
+  function handleDelete(id){
+        callApi.deleteProduct(id)
+        .then(function(){
+            getListProduct();
+        })  
+        .catch(function(error){
+            console.log(error);
+        });
+    }
+    getEle("btnThemSP").addEventListener("click",function(){
+        document.getElementsByClassName("modal-title")[0].innerHTML="Add";
+        var btnAdd = `<button class= "btn btn-success" onclick="handleAdd()">Add</button>`;
+        document.getElementsByClassName("modal-footer")[0].innerHTML =btnAdd;
+    });
+    function handleEdit(id){
+        // update title
+        document.getElementsByClassName("modal-title")[0].innerHTML = "Edit";
+    
+        var btnUpdate = `<button class="btn btn-success" onclick="handleUpdate(${id})">Update</button>`;
+        document.getElementsByClassName("modal-footer")[0].innerHTML=btnUpdate;
+    
+    callApi.getProductById(id)
+    .then(function(result){
+        var product = result.data;
+        getEle("name").value = product.name;
+        getEle("price").value = product.price;
+        getEle("screen").value = product.screen;
+        getEle("backCamera").value = product.backCamera;
+        getEle("frontCamera").value = product.frontCamera;
+        getEle("desc").value = product.desc;
+        getEle("type").value = product.type;
+
+// cap nhat hinh anh
+hinhAnhFromServer = product.img;
+    })  
+    .catch(function(error){
+console.log(error);
+    })
+    }
+    // update product
+    function handleUpdate(){
+            var name = getEle("name").value;
+            var price = getEle("price").value;
+            var screen = getEle("screen").value;
+            var backCamera = getEle("backCamera").value;
+            var frontCamera = getEle("frontCamera").value;
+            var desc = getEle("desc").value;
+            var type = getEle("type").value;
+        if(getEle("img").files.length>0){
+            img = getEle("img").file[0].name;
+        }
+        if(!img){
+            img = hinhAnhFromServer;
+        }
+        var product = new Product(id, name, price , screen , backCamera, frontCamera,desc,type);
+        callApi.updateProduct(product)
+        .then(function(result){
+            console.log(result.data);
+            getListProduct();
+            document.getElementsByClassName("close")[0].click();
+            hinhAnhFromServer="";
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+    }
+// add product
+function handleAdd(){
+    var name = getEle("name").value;
+            var price = getEle("price").value;
+            var screen = getEle("screen").value;
+            var backCamera = getEle("backCamera").value;
+            var frontCamera = getEle("frontCamera").value;
+            var desc = getEle("desc").value;
+            var type = getEle("type").value;
+        if(getEle("img").files.length>0){
+            img = getEle("img").file[0].name;
+        }
+        var product = new Product("",name,price,screen , backCamera, frontCamera,desc,type);
+        callApi.addProduct(product)
+        .then(function(result){
+            console.log(result.data);
+            getListProduct();
+            document.getElementsByClassName("close")[0].click();    
+        }) .catch(function(error){
+            console.log(error);
+        });
+}
--- app/services/callapi.js
+++ app/services/callapi.js
@@ -5,4 +5,30 @@ function CallApi(){
             method: "GET",
         });
     };
+        this.deleteProduct = function(id){
+            axios({
+                url:`https://63df6ff78b24964ae0edc536.mockapi.io/api/product?fbclid=IwAR125H7ylQm0B1LTt7lFecEdQ8mJJRcWkMGYEd-XZA9RoWJLr-N1uXnd_tg/products/${id}`,
+                method: "DELETE",
+            });
+        };
+        this.addProduct = function(product){
+            return axios({
+                url:"https://63df6ff78b24964ae0edc536.mockapi.io/api/product?fbclid=IwAR125H7ylQm0B1LTt7lFecEdQ8mJJRcWkMGYEd-XZA9RoWJLr-N1uXnd_tg/products",
+                method:"POST",
+                data : product,
+            });
+        };
+        this.getProductById = function(id){
+            return axios({
+                url:`https://63df6ff78b24964ae0edc536.mockapi.io/api/product?fbclid=IwAR125H7ylQm0B1LTt7lFecEdQ8mJJRcWkMGYEd-XZA9RoWJLr-N1uXnd_tg/products/${id}`,
+            method: "GET",
+            });
+        };
+      this.updateProduct = function(product){
+        return axios({
+            url:`https://63df6ff78b24964ae0edc536.mockapi.io/api/product?fbclid=IwAR125H7ylQm0B1LTt7lFecEdQ8mJJRcWkMGYEd-XZA9RoWJLr-N1uXnd_tg/products/${product.id}`,
+            method: "PUT",
+            data:product,
+        });
+      }  ;
 }
